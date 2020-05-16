d59b88664be1a19c956d05edd4ec406ff47d197e
yandersantana <ysantanatavares.1996@hotmail.com>
2020-05-16 13:01:52 -0500

    yaa

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // configure the code below with your username, password and mlab database information
+  database: 'mongodb+srv://admin:admin@cluster0-rkmn7.mongodb.net/test?retryWrites=true&w=majority',   //prod
+  //database: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: 'yoursecret'
+}
