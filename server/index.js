6ff715c37cbc119b0cd7ef083fb6de18ab0d940e
yandersantana <ysantanatavares.1996@hotmail.com>
2020-05-16 10:31:44 -0500

    subiendo2

--- server/index.js
+++ server/index.js
@@ -6,10 +6,10 @@ const app = express();
 const { mongoose } = require('./database');
 
 // Settings
-app.set('port', process.env.PORT || 3000);
+//app.set('port', process.env.PORT || 3000);
 
 // Middlewares
-app.use(morgan("dev"));
+/* app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors({origin:'http://localhost:4200'}));
 
@@ -20,3 +20,28 @@ app.use('/api/employees', require('./routes/employee.routes'));
 app.listen(app.get('port'), () => {
     console.log(`server on port ${app.get('port')}`);
 });
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+ */
+// Conectar Mongo
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb+srv://admin:admin@digifiletest1-o6t78.mongodb.net/test?retryWrites=true&w=majority', {
+   // useNewUrlParser: true
+})
+
+// Habilitar pug
+app.set('view engine', 'pug');
+
+// Carpeta para las vistas
+app.set('views', path.join(__dirname, './views'));
+
+// Cargar los archivos estaticos en public
+app.use(express.static('public'));
+
+// Definir rutas de la aplicación
+app.use('/api/employees', require('./routes/employee.routes'));
+
+
+app.listen(3000);
