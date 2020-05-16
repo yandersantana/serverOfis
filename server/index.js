eb6f7f4b26d7232738daadab3c171eeff14a1317
yandersantana <ysantanatavares.1996@hotmail.com>
2020-05-16 11:59:36 -0500

    kjkj

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/* const express = require('express');
 const app = express();
 const cors= require('cors');
 
@@ -19,7 +19,24 @@ app.use('/grupos', require('./routes/groupsRoutes'));
 app.listen(app.get('port'), () => {
    console.log('Server on port 3000, Yes');
 });
-
+ */
 /*app.get('', (req, res) => {
     res.send('Hola Mundo');
     });*/
+
+
+const app = express();
+const server= http.createServer(app);
+const io= socketio.listen(server);
+
+
+mongoose.connect('mongodb+srv://admin:admin@digifiletest1-o6t78.mongodb.net/test?retryWrites=true&w=majority').then(db => console.log("db connect"));
+
+
+app.set("port" . process.env.PORT || 3000);
+app.use('/api', require('./routes/index'));
+app.use('/grupos', require('./routes/groupsRoutes'));
+
+server.listen(app.get("port"),() => {
+   console.log("escuchanod en el puerti")
+})
