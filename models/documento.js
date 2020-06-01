8c35d3e2a7022b2a447fda95a8eede0cd00f2f7e
yandersantana <ysantanatavares.1996@hotmail.com>
2020-06-01 08:15:06 -0500

    subiendo

---
+++ models/documento.js
@@ -0,0 +1,31 @@
+
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const DocumentoSchema = new Schema({
+    id: { type: Number, required: false},
+    clase: { type: String, required: false},
+    nombreDocumento: { type: String, required: false},
+    version: { type: String, required: false},
+    urlDocumento: { type: String, required: false},
+    creadoPor: { type: String, required: false},
+    fechaCreacion: { type: String, required: false},
+    tamanoArchivo: { type: String, required: false},
+    newdoc_indices: { type: Array, required: false}
+    /* clasedoc_name: { type: String, required: false},
+    clasedoc_description: { type: String, required: false},
+    clasedoc_empresa: { type: String, required: false},
+    clasedoc_tamanoMax: { type: String, required: false},
+    clasedoc_cantidadMax: { type: Number, required: false},
+    clasedoc_cantidadDoc: { type: Number, required: false},
+    clasedoc_indicesNum: { type: Number, required: false},
+    clasedoc_estado: { type: String, required: false},
+    newdoc_indices: { type: Array, required: false} */
+   
+},{
+    timestamps:true
+});
+
+module.exports = mongoose.model('Documentos', DocumentoSchema);
+
+
--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -2,7 +2,8 @@
 const { Router } = require('express');
 const router = Router();
 const multipart = require('connect-multiparty');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const Documentos = require('../models/documento')
 
 
 //router.use(bodyParser.json());
@@ -21,6 +22,58 @@ router.post('/uploadFile', multipartMiddleware, (req, res, next) => {
     res.json({
         'message': 'File uploaded succesfully.'
     });
+});
+
+
+router.get('/getDocumentos', async (req, res) => {
+    const documentos = await Documentos.find();
+    res.send(documentos)      
+})
+
+router.get('/getDocumentos3/:clase', async (req, res) => {
+    const { clase } = req.params;
+    const clases = await Documentos.find({"clase":clase});
+    res.json(clases); 
+})
+
+router.put('/update/:id', async (req, res,next) => {
+    const { id } = req.params;
+    const newDoc = {
+        clasedoc_name: req.body.clasedoc_name,
+        clasedoc_description: req.body.clasedoc_description,
+        clasedoc_empresa: req.body.clasedoc_empresa,
+        clasedoc_tamanoMax: req.body.clasedoc_tamanoMax,
+        clasedoc_cantidadMax: req.body.clasedoc_cantidadMax,
+        clasedoc_cantidadDoc: req.body.clasedoc_cantidadDoc,
+        clasedoc_indicesNum: req.body.clasedoc_indicesNum,
+        clasedoc_estado: req.body.clasedoc_estado,
+        newdoc_indices: req.body.newdoc_indices
+    };
+    await Documentos.findByIdAndUpdate(id, {$set: newDoc}, {new: true});
+    res.json({status: 'Documentos Updated'});  
+})
+
+
+router.delete('/delete/:id', async (req, res,next) => {
+    await Documentos.findByIdAndRemove(req.params.id);
+    res.json({status: 'Documentos Deleted'});
+})
+
+
+router.post('/newDocument', async (req, res) => {
+    const newDoc = new Documentos({ 
+        clase: req.body.clase,
+        nombreDocumento: req.body.nombreDocumento,
+        version: req.body.version,
+        urlDocumento: req.body.urlDocumento,
+        creadoPor: req.body.creadoPor,
+        fechaCreacion: req.body.fechaCreacion,
+        tamanoArchivo: req.body.tamanoArchivo,
+        newdoc_indices: req.body.newdoc_indices});
+    console.log("ttt "+newDoc);
+    await newDoc.save();
+    //res.send("empresa registrado");
+    res.json({status: 'Documento CREADo'});
 });
 
 
