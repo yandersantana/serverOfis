b8484aa3585537dcd8fdfc2345a4fcb53fbf49e1
yandersantana <ysantanatavares.1996@hotmail.com>
2020-06-25 22:28:05 -0500

    subir

--- models/clasesDoc.js
+++ models/clasesDoc.js
@@ -9,7 +9,7 @@ const ClasesDocSchema = new Schema({
     clasedoc_empresa: { type: String, required: false},
     clasedoc_cantidadDoc: { type: Number, required: true},
     clasedoc_indicesNum: { type: Number, required: true},
-    clasedoc_tamanoMax: { type: String, required: true},
+    clasedoc_tamanoMax: { type: Number, required: true},
     clasedoc_cantidadMax: { type: Number, required: true},
     clasedoc_estado: { type: String, required: true},
     clasedoc_indices: { type: Array, required: false},
