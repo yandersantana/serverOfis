013ec6b957758a73dd252c132d811e390d2b8b82
kehylaicaza <keilacicaza@gmail.com>
2020-08-04 21:11:47 -0500

    acces

--- app.js
+++ app.js
@@ -23,25 +23,18 @@ const users = require('./routes/users');
 // Port Number
 const port = process.env.PORT || 3000;
 
-// CORS Middleware
-app.use(cors());
 
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
-
 app.use(bodyParser.json({limit: '50mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use('/uploads', express.static('uploads'));
+
+// CORS Middleware
+app.use(cors());
+
 // Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
