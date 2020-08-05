3cf6a50519a5a4b127658d34953e55bd11c8a0c9
kehylaicaza <keilacicaza@gmail.com>
2020-08-04 21:09:05 -0500

    acces

--- app.js
+++ app.js
@@ -30,8 +30,17 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+
+
 app.use(bodyParser.json({limit: '50mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/uploads', express.static('uploads'));
 // Passport Middleware
 app.use(passport.initialize());
